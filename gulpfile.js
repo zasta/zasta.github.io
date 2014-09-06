4ae3e46967b3a92ef6953f84100b576815a31917
Zasta <info@zasta.info>
2014-09-06 22:59:18 +0000

    Using a build tool to improve website performance

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp    = require('gulp'),
+    cssmin  = require('gulp-minify-css'),
+    htmlmin = require('gulp-htmlmin'),
+    imgmin  = require('gulp-imagemin');
+
+gulp.task('default', function() {
+    gulp.src('src/css').pipe(cssmin()).pipe(gulp.dest("css"));
+    gulp.src('src/*.html').pipe(htmlmin()).pipe(gulp.dest("."));
+    gulp.src('src/img/*').pipe(imgmin({progressive: true})).pipe(gulp.dest("img"));
+});
