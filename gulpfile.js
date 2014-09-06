fe41fffd12f7faac3cf51389f2cfa86494b91efc
Zasta <info@zasta.info>
2014-09-06 23:29:18 +0000

    Adding the concat task and update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp    = require('gulp'),
     cssmin  = require('gulp-minify-css'),
     htmlmin = require('gulp-htmlmin'),
-    imgmin  = require('gulp-imagemin');
+    imgmin  = require('gulp-imagemin'),
+    concat  = require('gulp-concat');
 
 gulp.task('default', function() {
-    gulp.src('src/css/*').pipe(cssmin({benchmark: true})).pipe(gulp.dest("./css"));
-    gulp.src('src/*.html').pipe(htmlmin()).pipe(gulp.dest("./"));
-    gulp.src('src/img/*').pipe(imgmin({progressive: true})).pipe(gulp.dest("./img"));
+    gulp.src('src/css/*').pipe(cssmin({benchmark: true})).pipe(gulp.dest('./css'));
+    gulp.src('src/*.html').pipe(htmlmin({collapseWhitespace: true, removeRedundantAttributes: true})).pipe(gulp.dest('./'));
+    gulp.src('src/img/*').pipe(imgmin({progressive: true})).pipe(gulp.dest('./img'));
+    gulp.src('css/*').pipe(concat('page.css')).pipe(gulp.dest('./css/'));
 });
