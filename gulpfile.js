8880808eb2ba0a75726ae068fbce4b0e57d9e8ed
Zasta <info@zasta.info>
2014-09-06 22:59:48 +0000

    Adjusting gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp    = require('gulp'),
     imgmin  = require('gulp-imagemin');
 
 gulp.task('default', function() {
-    gulp.src('src/css').pipe(cssmin()).pipe(gulp.dest("css"));
-    gulp.src('src/*.html').pipe(htmlmin()).pipe(gulp.dest("."));
-    gulp.src('src/img/*').pipe(imgmin({progressive: true})).pipe(gulp.dest("img"));
+    gulp.src('src/css/*').pipe(cssmin({benchmark: true})).pipe(gulp.dest("./css"));
+    gulp.src('src/*.html').pipe(htmlmin()).pipe(gulp.dest("./"));
+    gulp.src('src/img/*').pipe(imgmin({progressive: true})).pipe(gulp.dest("./img"));
 });
