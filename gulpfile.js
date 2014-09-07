fcc7cbdb7691b3aebce1331ea8a9261656a0cbb0
Zasta <info@zasta.info>
2014-09-07 11:57:43 +0000

    Updated page bg, footer css and build process

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ var gulp    = require('gulp'),
     concat  = require('gulp-concat');
 
 gulp.task('default', function() {
-    gulp.src('src/css/*').pipe(cssmin({benchmark: true})).pipe(gulp.dest('./css'));
+    gulp.src('src/css/*').pipe(cssmin({benchmark: true})).pipe(gulp.dest('./dist-css'));
     gulp.src('src/*.html').pipe(htmlmin({collapseWhitespace: true, removeRedundantAttributes: true})).pipe(gulp.dest('./'));
     gulp.src('src/img/*').pipe(imgmin({progressive: true})).pipe(gulp.dest('./img'));
-    gulp.src('css/*').pipe(concat('page.css')).pipe(gulp.dest('./css/'));
+    gulp.src('dist-css/*.css').pipe(concat('page.css')).pipe(gulp.dest('./css/'));
 });
